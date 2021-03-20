94c9dcd8d5b5ea1ba8d344770b2cbf0a4829f74b
Robert Reihs <robert.reihs@medunigraz.at>
2021-03-20 16:14:39 +0100

    Update API App errors in example

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import render_template
 from models import Participant, ParticipantSchema
 from jproperties import Properties
+
 import config
 
-configs = Properties()
+configsProperty = Properties()
 with open('config.properties', 'rb') as config_file:
-    configs.load(config_file)
+    configsProperty.load(config_file)
 
 # Get the application instance
 connex_app = config.connex_app
 # Read the swagger.yml file to configure the endpoints
 connex_app.add_api("swagger.yml")
+app = connex_app.app
 
 
 @connex_app.route('/')
@@ -19,4 +21,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    connex_app.run(host=configs.get("HOST").data, debug=configs.get("DEBUG").data)
+    connex_app.run(host=configsProperty.get("HOST").data, debug=configsProperty.get("DEBUG").data)
--- config.py
+++ config.py
@@ -6,9 +6,9 @@ from flask_marshmallow import Marshmallow
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-configs = Properties()
+configsProperty = Properties()
 with open('config.properties', 'rb') as config_file:
-    configs.load(config_file)
+    configsProperty.load(config_file)
 
 # Create the connexion application instance
 connex_app = connexion.App(__name__, specification_dir=basedir)
@@ -19,7 +19,7 @@ app.config["template_folder"] = "templates"
 
 # Configure the SqlAlchemy part of the app instance
 app.config["SQLALCHEMY_ECHO"] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = configs.get("SQLALCHEMY_DATABASE_URI").data
+app.config["SQLALCHEMY_DATABASE_URI"] = configsProperty.get("SQLALCHEMY_DATABASE_URI").data
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # Create the SqlAlchemy db instance
